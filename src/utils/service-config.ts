2421c9f42de5c9daa273bd27f8282ce78ed0d3a3
minh160302 <minh160302@gmail.com>
2021-07-26 20:10:33 +0700

    add heroku backend

--- src/utils/service-config.ts
+++ src/utils/service-config.ts
@@ -1,5 +1,6 @@
 export const SERVICE_CONFIG = {
-  SERVICE_API_URL: "http://localhost:8000",
+  // SERVICE_API_URL: "http://localhost:8000",
+  SERVICE_API_URL: "https://service-mstc.herokuapp.com",
   AUTH_SERVICE: "/auth-service",
   HEROKU_AUTH: "https://lit-shelf-16454.herokuapp.com",
 }
